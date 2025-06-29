7abb7c7d73088302604e7593999d756f9c311381
Aakash <suryapadayachi58@gmail.com>
2025-06-29 09:35:57 +0000

    see below the chat input there empty area check other elements

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -2,7 +2,7 @@ import ChatInterface from '@/components/chat-interface';
 
 export default function ChatPage() {
   return (
-    <div className="h-full w-full flex flex-col">
+    <div className="flex-1 w-full flex flex-col">
       <ChatInterface />
     </div>
   );
