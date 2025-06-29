d497cbe203014164334b6ee6ecd9bd79a6f725c7
Aakash <suryapadayachi58@gmail.com>
2025-06-29 16:07:04 +0000

    see two lines showing

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -30,7 +30,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn(className)}
     {...props}
   />
 ))
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted h-11",
+      "transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted h-11",
       className
     )}
     {...props}
