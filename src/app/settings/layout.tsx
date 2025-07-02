521b50489af644e6bdb3a96263a2dff2db83f545
Aakash <suryapadayachi58@gmail.com>
2025-07-02 13:04:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SettingsHeader>

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,4 +1,4 @@
-import SettingsHeader from '@/components/settings-header';
+import PublicHeader from '@/components/public-header';
 import React from 'react';
 
 export default function SettingsLayout({
@@ -8,7 +8,7 @@ export default function SettingsLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <SettingsHeader />
+      <PublicHeader />
       <main className="flex-1 py-8">
         <div className="container mx-auto">
             <div className="w-full max-w-5xl mx-auto">
