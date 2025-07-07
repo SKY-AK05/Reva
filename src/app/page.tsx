d5636278d4b38822ddd6ed7e6f9979dcb7608e6e
Aakash <suryapadayachi58@gmail.com>
2025-07-07 15:55:45 +0000

    now just make image bigger little bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -174,13 +174,13 @@ export default function HomePage() {
                   </CardContent>
                 </Card>
                 
-                <Card className="relative rounded-2xl shadow-lg h-full min-h-[280px] lg:col-span-2 lg:row-span-2">
+                <Card className="relative rounded-2xl shadow-lg h-full min-h-[280px] lg:col-span-2 lg:row-span-2 overflow-visible">
                     <Image
                         src="/assets/women.png"
                         alt="A woman happily using the Reva productivity app on her phone"
                         layout="fill"
                         objectFit="contain"
-                        className="scale-[1.25] lg:-translate-y-12"
+                        className="scale-[1.4] lg:-translate-y-12"
                         data-ai-hint="woman using phone"
                         quality={100}
                     />
