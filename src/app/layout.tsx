e7b391dcf1d53c9719d36f502244f3a6abd5f23a
SKY-AK05 <aakash20030501@gmail.com>
2025-07-09 22:51:56 +0530

    final update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/svg+xml" href="/assets/Dark Logo.svg" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
